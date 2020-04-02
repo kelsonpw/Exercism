6ee21962dd2a37d66237377b09276beb0479e397
Kelson Warner <kelson.warner@fundingcircle.com>
2020-04-02 07:57:30 -0700

    readd elixir

---
+++ elixir/list-ops/lib/list_ops.ex
@@ -0,0 +1,33 @@
+defmodule ListOps do
+  def count([]), do: 0
+  def count([_ | tail]), do: 1 + count(tail)
+
+  def reverse(list), do: reverse(list, [])
+
+  def reverse([], reversed), do: reversed
+  def reverse([head | tail], reversed), do: reverse(tail, [head | reversed])
+
+  def map([], _), do: []
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+
+  def filter([], _), do: []
+
+  def filter([head | tail], pred) do
+    if pred.(head) do
+      [head | filter(tail, pred)]
+    else
+      filter(tail, pred)
+    end
+  end
+
+  def reduce([], acc, _), do: acc
+
+  def reduce([head | tail], acc, func) do
+    reduce(tail, func.(head, acc), func)
+  end
+
+  def append(list_a, list_b), do: list_a ++ list_b
+
+  def concat([]), do: []
+  def concat([head | tail]), do: head ++ concat(tail)
+end
